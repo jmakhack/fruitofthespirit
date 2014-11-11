b7394923632d9abdb6ed5de05b9c344c57f14621
Joshua Mak <jmak2015@gmail.com>
2014-11-11 01:41:49 -0800

    Add tab animation

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,4 +6,32 @@ angular.module('fruitsofthespiritApp')
     $http.get('/api/fruits').success(function(fruits) {
       $scope.fruits = fruits;
     });
+
+    $(document).ready(function() {
+		var tabOpen = null;
+		var curText = $('.title');
+		var speed = 800;
+
+		$('.fruit').hover(function() {
+			if(parseInt($(this).css('left')) === -200)
+				$(this).animate({left: "+=10"}, 0, function() {});
+		}, function() {
+			if(parseInt($(this).css('left')) === -190)
+				$(this).animate({left: "-=10"}, 0, function() {});
+		});
+
+		$('.fruit').click(function() {
+			if(parseInt($(this).css('left')) === -190) {
+				if(tabOpen != null) {
+					tabOpen.animate({left: "-=200"}, speed, function() {});
+				}
+				$(this).animate({left: "+=190"}, speed, function() {});
+				tabOpen = $(this);
+			} else if(parseInt($(this).css('left')) === 0) {
+				tabOpen.animate({left: "-=200"}, speed, function() {});
+				tabOpen = null;
+			}
+		});
+	});
   });
+
