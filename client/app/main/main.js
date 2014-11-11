fb0be0e16cf9b18497ef2ba5589f4add8013f08f
Joshua Mak <jmak2015@gmail.com>
2014-11-10 17:15:49 -0800

    Setup MEAN stack scaffolding

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fruitsofthespiritApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
